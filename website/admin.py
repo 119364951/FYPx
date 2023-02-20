8ec17a2580ffed9b6da0507776b01b368b73589e
119364951 <119364951@umail.ucc.ie>
2023-02-20 21:38:46 +0000

    IT5-MK2

--- website/admin.py
+++ website/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 #Code derived from Python Django Tutorial: Full-Featured Web App Part 5 - Database and Migrations
 #Timestamp 35:46
-from .models import Posts
+# Code derived from "Build A Blog Comment Section - Django Blog #33" Timestamp 6:44
+from .models import Posts, Comments
 
 admin.site.register(Posts)
+admin.site.register(Comments)
--- website/models.py
+++ website/models.py
@@ -15,6 +15,14 @@ class Posts(models.Model):
     # If User gets deleted their posts get removed too
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+# Code derived from "Build A Blog Comment Section - Django Blog #33" Timestamp 1:40
+class Comments(models.Model):
+    posts = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+
 #Derived Python Django Tutorial: Full-Feautred Web App Part 5 timestamp 19:39
 #Code updated from "Python Django Tutorial: Full-Featured Web App Part 10 - Create, Update, and Delete Posts" Timestamp 28:25
     def __str__(self):
