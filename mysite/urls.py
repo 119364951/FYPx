1bf370bdfdc761de98d950e27be88bb8f3e57ef8
119364951 <119364951@umail.ucc.ie>
2023-03-05 17:36:22 +0000

    IT6-MK10

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,6 +31,7 @@ from django.conf.urls.static import static
 # Connects the project urls to the app urls
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
+    path('', include('website.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
